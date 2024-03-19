165f30facb6ddfa3bc5369fde388eb92879119fb
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-03-19 09:30:41 +0100

    finshed

---
+++ Future Value/Contollers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Future_Value.Models;
+namespace Future_Value.Contollers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Faith";
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel futurevalue)
+        {
+            if(ModelState.IsValid)
+            {
+            ViewBag.FV = futurevalue.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.Fv = 0;
+            }
+            return View(futurevalue);
+        }
+    }
+}
